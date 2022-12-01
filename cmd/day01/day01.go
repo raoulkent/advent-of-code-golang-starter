141c9b4df138d4e79bdfdc71581f8ed21e47da9b
raoulkent <raoulkent@gmail.com>
2022-12-01 16:03:48 +0100

    day1: solve puzzle 1

--- cmd/day01/day01.go
+++ cmd/day01/day01.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
+	"strconv"
+	"strings"
 
 	"aoc/internal/utils"
 )
@@ -22,13 +25,85 @@ func main() {
 	os.Exit(0)
 }
 
-// part one
-func part1(input string) int {
-	return 0
+type elfCandyBag struct {
+	elf        int
+	candyTotal int
 }
 
-// part two
+type elfCandyBags []elfCandyBag
+
+func (e elfCandyBags) Len() int {
+	return len(e)
+}
+
+func (e elfCandyBags) Less(i, j int) bool {
+	return e[i].candyTotal < e[j].candyTotal
+}
+
+func (e elfCandyBags) Swap(i, j int) {
+	e[i], e[j] = e[j], e[i]
+}
+
+// ██████╗  █████╗ ██████╗ ████████╗     ██████╗ ███╗   ██╗███████╗
+// ██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝    ██╔═══██╗████╗  ██║██╔════╝
+// ██████╔╝███████║██████╔╝   ██║       ██║   ██║██╔██╗ ██║█████╗
+// ██╔═══╝ ██╔══██║██╔══██╗   ██║       ██║   ██║██║╚██╗██║██╔══╝
+// ██║     ██║  ██║██║  ██║   ██║       ╚██████╔╝██║ ╚████║███████╗
+// ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝        ╚═════╝ ╚═╝  ╚═══╝╚══════╝
+
+func part1(input string) int {
+	candyBags := bagStringsToElfCandyBags(input)
+
+	sort.Sort(sort.Reverse(candyBags))
+	fmt.Printf("Elf with the most candy: %d \n", candyBags[0].elf)
+
+	return candyBags[0].candyTotal
+}
+
+func bagStringsToElfCandyBags(input string) elfCandyBags {
+	var candyBags elfCandyBags
+
+	bagStrings := strings.Split(input, "\n\n")
+
+	for i, s := range bagStrings {
+		candy := strings.Split(s, "\n")
+		candySum := sumArr(convertArrStringToArrInt(candy))
+
+		// Add the elf i, and the candyTotal candySum to the candyBags
+		candyBags = append(candyBags, elfCandyBag{i, candySum})
+	}
+
+	return candyBags
+}
+
+func convertArrStringToArrInt(input []string) []int {
+	var output []int
+
+	for _, v := range input {
+		value, _ := strconv.Atoi(v)
+		output = append(output, value)
+	}
+
+	return output
+}
+
+func sumArr(input []int) int {
+	var sum int
+
+	for _, v := range input {
+		sum += v
+	}
+
+	return sum
+}
+
+// ██████╗  █████╗ ██████╗ ████████╗    ████████╗██╗    ██╗ ██████╗
+// ██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝    ╚══██╔══╝██║    ██║██╔═══██╗
+// ██████╔╝███████║██████╔╝   ██║          ██║   ██║ █╗ ██║██║   ██║
+// ██╔═══╝ ██╔══██║██╔══██╗   ██║          ██║   ██║███╗██║██║   ██║
+// ██║     ██║  ██║██║  ██║   ██║          ██║   ╚███╔███╔╝╚██████╔╝
+// ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝          ╚═╝    ╚══╝╚══╝  ╚═════╝
+
 func part2(input string) int {
 	return 0
 }
-
