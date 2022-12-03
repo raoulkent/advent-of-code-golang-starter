3f38b96ffec1f64d6620a49c22ebacd16143f8f8
raoulkent <raoulkent@gmail.com>
2022-12-03 14:14:13 +0100

    day03: create two way maps between runes and binary repr

--- cmd/day03/day03.go
+++ cmd/day03/day03.go
@@ -22,9 +22,68 @@ func main() {
 	os.Exit(0)
 }
 
+// ███████╗██╗   ██╗██████╗ ██████╗  ██████╗ ██████╗ ████████╗
+// ██╔════╝██║   ██║██╔══██╗██╔══██╗██╔═══██╗██╔══██╗╚══██╔══╝
+// ███████╗██║   ██║██████╔╝██████╔╝██║   ██║██████╔╝   ██║
+// ╚════██║██║   ██║██╔═══╝ ██╔═══╝ ██║   ██║██╔══██╗   ██║
+// ███████║╚██████╔╝██║     ██║     ╚██████╔╝██║  ██║   ██║
+// ╚══════╝ ╚═════╝ ╚═╝     ╚═╝      ╚═════╝ ╚═╝  ╚═╝   ╚═╝
+
+func createRunesAsBinary() map[rune]int {
+	// create a map from rune to int representation where the rune is a-Z
+	// and the int representation is 2^0, 2^1, 2^2, etc.
+	var runesAsBinary = make(map[rune]int)
+
+	for i, r := range "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" {
+		runesAsBinary[r] = 1 << i // 2^i
+	}
+
+	return runesAsBinary
+}
+
+func createBinaryAsRunes() map[int]rune {
+	// create a map from int representation to rune where the int representation is 2^0, 2^1, 2^2, etc.
+	// and the rune is a-Z
+	var binaryAsRunes = make(map[int]rune)
+
+	for i, r := range "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" {
+		binaryAsRunes[1<<i] = r // 2^i
+	}
+
+	return binaryAsRunes
+}
+
+func reduce(input string, runeMap map[rune]int) int {
+	// reduce the input string to a single int value
+	// by ORing all the rune values together
+	var result int
+
+	for _, r := range input {
+		result |= runeMap[r]
+	}
+
+	return result
+}
+
+// ██████╗  █████╗ ██████╗ ████████╗ ██╗
+// ██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝███║
+// ██████╔╝███████║██████╔╝   ██║   ╚██║
+// ██╔═══╝ ██╔══██║██╔══██╗   ██║    ██║
+// ██║     ██║  ██║██║  ██║   ██║    ██║
+// ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝    ╚═╝
+
 func part1(input string) int {
+	// runesAsBinary := createRunesAsBinary()
+	// binaryAsRunes := createBinaryAsRunes()
 	return 0
 }
+
+// ██████╗  █████╗ ██████╗ ████████╗██████╗
+// ██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝╚════██╗
+// ██████╔╝███████║██████╔╝   ██║    █████╔╝
+// ██╔═══╝ ██╔══██║██╔══██╗   ██║   ██╔═══╝
+// ██║     ██║  ██║██║  ██║   ██║   ███████╗
+// ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚══════╝
 
 func part2(input string) int {
 	return 0
