d5d1c8bcd12e948c479fd9153a8c28565e178f18
raoulkent <raoulkent@gmail.com>
2022-12-03 14:48:52 +0100

    day03: solve puzzle 1

--- cmd/day03/day03.go
+++ cmd/day03/day03.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"os"
+	"time"
 
 	"aoc/internal/utils"
 )
 
 func main() {
-	input, err := utils.ReadFile("resources/day03.txt")
+	input, err := utils.ReadBuffer("resources/day03.txt")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(0)
@@ -41,16 +44,16 @@ func createRunesAsBinary() map[rune]int {
 	return runesAsBinary
 }
 
-func createBinaryAsRunes() map[int]rune {
-	// create a map from int representation to rune where the int representation is 2^0, 2^1, 2^2, etc.
-	// and the rune is a-Z
-	var binaryAsRunes = make(map[int]rune)
+func createPriorities() map[int]int {
+	// create a map from int to int where the int is 2^0, 2^1, 2^2, etc.
+	// and the int is the priority 1, 2, 3, etc.
+	var priorities = make(map[int]int)
 
-	for i, r := range "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" {
-		binaryAsRunes[1<<i] = r // 2^i
+	for i, _ := range "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" {
+		priorities[1<<i] = i + 1 // 2^i
 	}
 
-	return binaryAsRunes
+	return priorities
 }
 
 func reduce(input string, runeMap map[rune]int) int {
@@ -72,10 +75,39 @@ func reduce(input string, runeMap map[rune]int) int {
 // ██║     ██║  ██║██║  ██║   ██║    ██║
 // ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝    ╚═╝
 
-func part1(input string) int {
-	// runesAsBinary := createRunesAsBinary()
-	// binaryAsRunes := createBinaryAsRunes()
-	return 0
+func part1(buffer *bufio.Reader) int {
+	start := time.Now()
+	var result int
+
+	runesAsBinary := createRunesAsBinary()
+	priorities := createPriorities()
+
+	for {
+		line, err := buffer.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		}
+
+		halfSize := len(line) / 2
+		leftSack := line[:halfSize]
+		rightSack := line[halfSize : len(line)-1]
+
+		leftSackReduced := reduce(leftSack, runesAsBinary)
+		rightSackReduced := reduce(rightSack, runesAsBinary)
+		filteredSacks := leftSackReduced & rightSackReduced
+		priority := priorities[filteredSacks]
+
+		result += priority
+	}
+
+	elapsed := time.Since(start)
+	fmt.Printf("Part 1 took %s", elapsed)
+	return result
 }
 
 // ██████╗  █████╗ ██████╗ ████████╗██████╗
@@ -85,6 +117,6 @@ func part1(input string) int {
 // ██║     ██║  ██║██║  ██║   ██║   ███████╗
 // ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚══════╝
 
-func part2(input string) int {
+func part2(buffer *bufio.Reader) int {
 	return 0
 }
