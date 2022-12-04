9d4040163909e075ebf613a1b7a306ed54287b31
raoulkent <raoulkent@gmail.com>
2022-12-04 09:20:30 +0100

    day04: solve puzzle 2

--- cmd/day04/day04.go
+++ cmd/day04/day04.go
@@ -20,16 +20,14 @@ func main() {
 		os.Exit(0)
 	}
 
-	// // read from http request
-	// session := os.Getenv("AOC_SESSION")
-	// input, err := utils.ReadHTTP(2021, 1, session)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	os.Exit(0)
-	// }
-
 	fmt.Println("--- Part One ---")
 	fmt.Println("Result:", part1(buffer))
+
+	buffer, err = utils.ReadBuffer("resources/day04.txt")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(0)
+	}
 	fmt.Println("--- Part Two ---")
 	fmt.Println("Result:", part2(buffer))
 
@@ -101,19 +99,41 @@ func part1(buffer *bufio.Reader) int {
 func part2(buffer *bufio.Reader) int {
 	start := time.Now()
 
-	// for {
-	// 	line, err := buffer.ReadString('\n')
-	// 	if err != nil {
-	// 		if err == io.EOF {
-	// 			break
-	// 		} else {
-	// 			fmt.Println(err)
-	// 			os.Exit(1)
-	// 		}
-	// 	}
-	// }
+	var count int
+
+	for {
+		line, err := buffer.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		}
+
+		line = strings.TrimSuffix(line, "\n")
+
+		pairs := strings.Split(line, ",")
+		var lower, upper []int
+		for _, pair := range pairs {
+			split := strings.Split(pair, "-")
+
+			leftInt, _ := strconv.Atoi(split[0])
+			rightInt, _ := strconv.Atoi(split[1])
+
+			lower = append(lower, leftInt)
+			upper = append(upper, rightInt)
+		}
+
+		if lower[0] <= lower[1] && lower[1] <= upper[0] {
+			count++
+		} else if lower[1] <= lower[0] && lower[0] <= upper[1] {
+			count++
+		}
+	}
 
 	elapsed := time.Since(start)
 	fmt.Printf("Part 2 took %s\n", elapsed)
-	return 0
+	return count
 }
