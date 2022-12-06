5f9df361e2a5493af2e3e69c65677e59ccd89a09
raoulkent <raoulkent@gmail.com>
2022-12-06 14:52:58 +0100

    day05: clean up

--- cmd/day05/day05.go
+++ cmd/day05/day05.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"time"
@@ -9,23 +10,30 @@ import (
 )
 
 func main() {
-	// read form file
-	input, err := utils.ReadFile("resources/day05.txt")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(0)
-	}
+	var input1, input2 *bufio.Reader
 
 	fmt.Println("--- Part One ---")
-	fmt.Println("Result:", part1(input))
+	input1, _ = utils.ReadBuffer("resources/day05.txt")
+	fmt.Println("Result:", part1(input1))
+
 	fmt.Println("--- Part Two ---")
-	fmt.Println("Result:", part2(input))
+	input2, _ = utils.ReadBuffer("resources/day05.txt")
+	fmt.Println("Result:", part2(input2))
 
 	os.Exit(0)
 }
 
-// part one
-func part1(input string) int {
+// ____ _  _ ___  ___  ____ ____ ___
+// [__  |  | |__] |__] |  | |__/  |
+// ___] |__| |    |    |__| |  \  |
+
+// Place support functions here
+
+// ___  ____ ____ ___    ____ _  _ ____
+// |__] |__| |__/  |     |  | |\ | |___
+// |    |  | |  \  |     |__| | \| |___
+
+func part1(buffer *bufio.Reader) int {
 	start := time.Now()
 
 	elapsed := time.Since(start)
@@ -33,8 +41,11 @@ func part1(input string) int {
 	return 0
 }
 
-// part two
-func part2(input string) int {
+// ___  ____ ____ ___    ___ _ _ _ ____
+// |__] |__| |__/  |      |  | | | |  |
+// |    |  | |  \  |      |  |_|_| |__|
+
+func part2(buffer *bufio.Reader) int {
 	start := time.Now()
 
 	elapsed := time.Since(start)
