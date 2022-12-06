21ff09a000ea913f5f70bbd82706c6611172d24b
raoulkent <raoulkent@gmail.com>
2022-12-06 09:49:30 +0100

    day05: add resource file

--- cmd/day05/day05.go
+++ cmd/day05/day05.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	// read form file
-	input, err := utils.ReadFile("resources/dayXX.txt")
+	input, err := utils.ReadFile("resources/day05.txt")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(0)
