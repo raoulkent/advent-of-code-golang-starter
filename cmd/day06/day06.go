39dd43140155dc69d500c5f486864c8b0fff6cb2
raoulkent <raoulkent@gmail.com>
2022-12-06 10:26:03 +0100

    day06: readfile > readbuffer

--- cmd/day06/day06.go
+++ cmd/day06/day06.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"time"
@@ -9,23 +10,21 @@ import (
 )
 
 func main() {
-	// read form file
-	input, err := utils.ReadFile("resources/day06.txt")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(0)
-	}
+	var input1, input2 *bufio.Reader
 
 	fmt.Println("--- Part One ---")
-	fmt.Println("Result:", part1(input))
+	input1, _ = utils.ReadBuffer("resources/day03.txt")
+	fmt.Println("Result:", part1(input1))
+
 	fmt.Println("--- Part Two ---")
-	fmt.Println("Result:", part2(input))
+	input2, _ = utils.ReadBuffer("resources/day03.txt")
+	fmt.Println("Result:", part2(input2))
 
 	os.Exit(0)
 }
 
 // part one
-func part1(input string) int {
+func part1(buffer *bufio.Reader) int {
 	start := time.Now()
 
 	elapsed := time.Since(start)
@@ -34,7 +33,7 @@ func part1(input string) int {
 }
 
 // part two
-func part2(input string) int {
+func part2(buffer *bufio.Reader) int {
 	start := time.Now()
 
 	elapsed := time.Since(start)
