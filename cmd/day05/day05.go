61d1dc5cfc218085d5fde48ea96d18d95bc3a283
raoulkent <raoulkent@gmail.com>
2022-12-06 08:40:09 +0100

    day05: init

---
+++ cmd/day05/day05.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"aoc/internal/utils"
+)
+
+func main() {
+	// read form file
+	input, err := utils.ReadFile("resources/dayXX.txt")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(0)
+	}
+
+	fmt.Println("--- Part One ---")
+	fmt.Println("Result:", part1(input))
+	fmt.Println("--- Part Two ---")
+	fmt.Println("Result:", part2(input))
+
+	os.Exit(0)
+}
+
+// part one
+func part1(input string) int {
+	start := time.Now()
+
+	elapsed := time.Since(start)
+	fmt.Printf("Part 1 took %s\n", elapsed)
+	return 0
+}
+
+// part two
+func part2(input string) int {
+	start := time.Now()
+
+	elapsed := time.Since(start)
+	fmt.Printf("Part 2 took %s\n", elapsed)
+	return 0
+}
