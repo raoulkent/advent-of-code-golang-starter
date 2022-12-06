f8b91aa2e498e38ae4eb4caed2007ff20871f503
raoulkent <raoulkent@gmail.com>
2022-12-06 11:07:04 +0100

    day06: solve puzzle 1

--- cmd/day06/day06.go
+++ cmd/day06/day06.go
@@ -13,26 +13,70 @@ func main() {
 	var input1, input2 *bufio.Reader
 
 	fmt.Println("--- Part One ---")
-	input1, _ = utils.ReadBuffer("resources/day03.txt")
+	input1, _ = utils.ReadBuffer("resources/day06.txt")
 	fmt.Println("Result:", part1(input1))
 
 	fmt.Println("--- Part Two ---")
-	input2, _ = utils.ReadBuffer("resources/day03.txt")
+	input2, _ = utils.ReadBuffer("resources/day06.txt")
 	fmt.Println("Result:", part2(input2))
 
 	os.Exit(0)
 }
 
-// part one
+// ███████╗██╗   ██╗██████╗ ██████╗  ██████╗ ██████╗ ████████╗
+// ██╔════╝██║   ██║██╔══██╗██╔══██╗██╔═══██╗██╔══██╗╚══██╔══╝
+// ███████╗██║   ██║██████╔╝██████╔╝██║   ██║██████╔╝   ██║
+// ╚════██║██║   ██║██╔═══╝ ██╔═══╝ ██║   ██║██╔══██╗   ██║
+// ███████║╚██████╔╝██║     ██║     ╚██████╔╝██║  ██║   ██║
+// ╚══════╝ ╚═════╝ ╚═╝     ╚═╝      ╚═════╝ ╚═╝  ╚═╝   ╚═╝
+
+func bytesAreUnique(bytes []byte) bool {
+	seen := make(map[byte]bool)
+	for _, r := range bytes {
+		if seen[r] {
+			return false
+		}
+		seen[r] = true
+	}
+	return true
+}
+
+// ██████╗  █████╗ ██████╗ ████████╗     ██████╗ ███╗   ██╗███████╗
+// ██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝    ██╔═══██╗████╗  ██║██╔════╝
+// ██████╔╝███████║██████╔╝   ██║       ██║   ██║██╔██╗ ██║█████╗
+// ██╔═══╝ ██╔══██║██╔══██╗   ██║       ██║   ██║██║╚██╗██║██╔══╝
+// ██║     ██║  ██║██║  ██║   ██║       ╚██████╔╝██║ ╚████║███████╗
+// ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝        ╚═════╝ ╚═╝  ╚═══╝╚══════╝
+
 func part1(buffer *bufio.Reader) int {
 	start := time.Now()
 
+	// The message begins after 4 characters (the header of the message)
+	var position int = 0 + 4
+
+	for {
+		peek, _ := buffer.Peek(4)
+		if bytesAreUnique(peek) {
+			fmt.Printf("Found unique bytes at position %d: %s\n", position, peek)
+			break
+		}
+
+		buffer.ReadRune()
+		position++
+	}
+
 	elapsed := time.Since(start)
 	fmt.Printf("Part 1 took %s\n", elapsed)
-	return 0
+	return position
 }
 
-// part two
+// ██████╗  █████╗ ██████╗ ████████╗    ████████╗██╗    ██╗ ██████╗
+// ██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝    ╚══██╔══╝██║    ██║██╔═══██╗
+// ██████╔╝███████║██████╔╝   ██║          ██║   ██║ █╗ ██║██║   ██║
+// ██╔═══╝ ██╔══██║██╔══██╗   ██║          ██║   ██║███╗██║██║   ██║
+// ██║     ██║  ██║██║  ██║   ██║          ██║   ╚███╔███╔╝╚██████╔╝
+// ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝          ╚═╝    ╚══╝╚══╝  ╚═════╝
+
 func part2(buffer *bufio.Reader) int {
 	start := time.Now()
 
